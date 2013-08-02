5f194a32d3c46bdbea996af0eacd3e913300385c
Jan Segre <jan@segre.in>
2013-08-02 19:36:00 -0300

    Static index.html generation from template with config.json
    A compile.js script was added to compile files from the template dir to
    the static dir.
    Variables to be used on the template are set on config.json

---
+++ compile.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+fs = require('fs')
+mustache = require('mustache')
+config = require('./config')
+
+var output_dir = 'static/';
+var input_dir = 'template/';
+var files = [
+	'index.html'
+];
+
+function compileTemplate(filepath) {
+	return function (err, data) {
+		fs.writeFile(
+			filepath,
+			mustache.render(data.toString(), config),
+			function(err) { if (err) throw err; else console.log('compiled: ' + filepath)}
+		)
+	};
+};
+
+files.forEach(function (file) {
+	fs.readFile(input_dir + file, compileTemplate(output_dir + file));
+});
