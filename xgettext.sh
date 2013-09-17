a2839ac7d18ab1f048b469be0e8045a58574e592
Jan Segre <jan@segre.in>
2013-09-17 18:19:48 -0300

    Improving xgettext script.

--- xgettext.sh
+++ xgettext.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-handlebars-xgettext -o locale/messages.pot -D template
+
+fail() {
+	echo "Failed on $@."
+	exit 1
+}
+
+handlebars-xgettext -o locale/messages.pot -D template > /dev/null || fail handlebars-xgettext
 
 #                   s/\//\\\//g
 abspath=`pwd | sed 's/\//\\\\\\//g'`
@@ -8,7 +14,9 @@ sed -i.tmp "s/$abspath\///" locale/messages.pot
 rm locale/messages.pot.tmp
 
 for lang in en br; do
-msgmerge locale/$lang/messages.po locale/messages.pot > locale/$lang/messages.po.new
-mv locale/$lang/messages.po.new locale/$lang/messages.po
-po2json locale/$lang/messages.po locale/$lang/messages.json
+	msgmerge -q locale/$lang/messages.po locale/messages.pot > locale/$lang/messages.po.new || fail msgmerge
+	mv locale/$lang/messages.po.new locale/$lang/messages.po
+	po2json locale/$lang/messages.po locale/$lang/messages.json || fail po2json
 done
+
+echo 'Done!'
